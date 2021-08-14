a9086efaca6483fadaa1a2556c2e20bfb4bf6a65
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2021-08-14 20:14:03 +0900

    first commit

---
+++ src/main/java/com/example/zipcodes/ZipcodesApplication.java
@@ -0,0 +1,13 @@
+package com.example.zipcodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipcodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipcodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zipcodes/ZipcodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zipcodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipcodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
